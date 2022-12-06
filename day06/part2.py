3cff595c87d696050314955ab39f9850516b0355
Alex Via <alexviacoll@gmail.com>
2022-12-05 21:14:09 -0800

    Day 6

---
+++ day06/part2.py
@@ -0,0 +1,20 @@
+def solve(s):
+    chars = []
+    n = 14
+    for i, c in enumerate(s):
+        if len(chars) != n - 1:
+            chars.append(c)
+        else:
+            chars.append(c)
+            if len(set(chars)) == n:
+                return i + 1
+
+            if len(chars) == n:
+                chars.pop(0)
+
+def main():
+    with open('input.txt') as f:
+        print(solve(f.read()))
+
+if __name__ == '__main__':
+    main()
